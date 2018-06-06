64c02d1abb5fc9731000ca7bdce35dcfb5794005
Amit Goldberg <amit.goldberg@gmail.com>
2018-06-06 22:27:54 +0100

    Add luas_cli Formula (#1)

---
+++ Formula/luas_cli.rb
@@ -0,0 +1,15 @@
+class LuasCli < Formula
+  desc "Luas (Dublin trams) CLI tool"
+  homepage "https://github.com/amitizle/luas-cli"
+  url "https://github.com/amitizle/luas-cli/releases/download/v1.0.7/luas_1.0.7_darwin_x86_64.tar.gz"
+  version "1.0.7"
+  sha256 "adccfe12d2a24b2d899a1207aff1640e207d5bf9cdf37c6c7061597f5ef009ca"
+
+  def install
+    bin.install "luas"
+  end
+
+  test do
+    system "#{bin}/luas --help"
+  end
+end

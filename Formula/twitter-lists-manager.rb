9482f19cbd31a2bc82727867d94b698ea677e070
Amit Goldberg <amit.goldberg@gmail.com>
2018-10-06 16:51:34 +0100

    Fix the link to version 0.1.4

--- Formula/twitter-lists-manager.rb
+++ Formula/twitter-lists-manager.rb
@@ -1,7 +1,7 @@
 class TwitterListsManager < Formula
   desc "Twitter lists manager CLI"
   homepage "https://github.com/amitizle/twitter-lists-manager"
-  url "https://github.com/amitizle/twitter-lists-manager/releases/download/0.1.3/twitter-lists-manager0.1.3_darwin_x86_64.tar.gz"
+  url "https://github.com/amitizle/twitter-lists-manager/releases/download/0.1.4/twitter-lists-manager0.1.4_darwin_x86_64.tar.gz"
   version "0.1.4"
   sha256 "c53c303805dcf510c67965933fca1816d811af973af0c31677150716ecaf950d"
 

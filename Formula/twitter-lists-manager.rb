b243151f0e6fad1ebc7993d945f82bb4e20ec87a
Amit Goldberg <amit.goldberg@gmail.com>
2018-10-06 16:43:11 +0100

    Update twitter-lists-manager

--- Formula/twitter-lists-manager.rb
+++ Formula/twitter-lists-manager.rb
@@ -2,8 +2,8 @@ class TwitterListsManager < Formula
   desc "Twitter lists manager CLI"
   homepage "https://github.com/amitizle/twitter-lists-manager"
   url "https://github.com/amitizle/twitter-lists-manager/releases/download/0.1.3/twitter-lists-manager0.1.3_darwin_x86_64.tar.gz"
-  version "0.1.3"
-  sha256 "8979c26a0c2736d0d08a522502f5097679922a79d446c85a5d1278c495f60f67"
+  version "0.1.4"
+  sha256 "c53c303805dcf510c67965933fca1816d811af973af0c31677150716ecaf950d"
 
   def install
     bin.install "twitter-lists-manager"
